e0b4bdbe035bdd4a9ea88d2415972bbc4c37a0df
mjclark1 <mattjclark0407@hotmail.com>
2018-05-13 16:19:33 +1000

    Initial import of license parser library

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='license_parser',
+    version='1.0.dev0',
+    description='Library for working with the geotool rest interface',
+    url='https://bitbucket.org/poslive/python-lib-license_parser.git',
+    author='Matt Clark',
+    author_email='mattjclark0407@hotmail.com',
+    license='Copyright (C) Matt Clark - All Rights Reserved',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    dependency_links=[
+    ],
+    zip_safe=True
+)

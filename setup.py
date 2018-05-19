95e0bd4fdae48bded13f95c628131634a37592fb
mjclark1 <mattjclark0407@hotmail.com>
2018-05-19 13:38:48 +1000

    Fix up package naming.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='license_parser',
+    name='license-parser',
     version='1.1.dev0',
     description='Library for working with the geotool rest interface',
-    url='https://bitbucket.org/poslive/python-lib-license_parser.git',
+    url='https://bitbucket.org/poslive/python-lib-license-parser.git',
     author='Matt Clark',
     author_email='mattjclark0407@hotmail.com',
     license='Copyright (C) Matt Clark - All Rights Reserved',
